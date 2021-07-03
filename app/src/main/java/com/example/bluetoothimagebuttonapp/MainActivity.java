6a09168961b037475fbec337443652d7d5e6d471
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 11:25:59 +0530

    update the value for the field bluetoothState

--- app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!bluetoothState) {
                     bluetoothAdapter.enable();
+                    bluetoothState = true;
                 }
             }
         });
