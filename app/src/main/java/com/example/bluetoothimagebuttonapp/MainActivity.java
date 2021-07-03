247c7494e9f79caa5f9f6256a792133eca7258f0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 11:19:22 +0530

    initialize the object bluetoothAdapter

--- app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageButton = (ImageButton)findViewById(R.id.imageButton);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 }
