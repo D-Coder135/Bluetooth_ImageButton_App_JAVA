de4fa345f116df17ffbac401828d92134124c6d8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 11:26:49 +0530

    add a else block

--- app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
                     bluetoothAdapter.enable();
                     bluetoothState = true;
                     imageButton.setImageResource(R.drawable.on);
+                } else {
+
                 }
             }
         });
