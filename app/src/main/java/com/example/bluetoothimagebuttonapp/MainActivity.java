0696c07b3bdee6e73b6cca0e383986a7f6f49e65
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 11:35:46 +0530

    remove unused import statements. Final Commit!

--- app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothimagebuttonapp/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
